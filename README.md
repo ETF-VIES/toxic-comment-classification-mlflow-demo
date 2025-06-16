6688e8e91172fd9d7081aef6e1ee044c5acebe26
Aldin Kapetanovic <aldinkapetanovic@gmail.com>
2025-06-16 15:04:00 +0200

    packed

--- README.md
+++ README.md
@@ -10,15 +10,21 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 mlflow ui
+```
 
+```sh
+source venv/bin/activate
 export MLFLOW_TRACKING_URI=http://localhost:5000
-
 python3 train.py
+```
 
+```sh
 vim serve.py
-
+source venv/bin/activate
 uvicorn serve:app --reload
+```
 
+```sh
 curl -X POST http://localhost:8000/predict \
   -H "Content-Type: application/json" \
   -d '{"text": "Screw you"}'
--- serve.py
+++ serve.py
@@ -6,8 +6,8 @@ import joblib
 app = FastAPI()
 
 # Replace this with your actual run ID from MLflow
-RUN_ID = "e5bd9b0902704c7d8a06a89544f0c8d2"
-MODEL_ID = "m-9fdc8ba945e04a9387aaa4f64438a8ad"
+RUN_ID = "f01bae019772482a98e68ccd8f366589"
+MODEL_ID = "m-cebe2487dda4438b8f78577270ec83ea"
 
 model = mlflow.sklearn.load_model(f"mlartifacts/0/models/{MODEL_ID}/artifacts")
 vectorizer = joblib.load(f"mlartifacts/0/{RUN_ID}/artifacts/vectorizer.pkl")
